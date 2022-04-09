e9c630ccdb2556000487a6e814fc3a22ab087d79
Randja Raharijaona <randzoh@outlook.com>
2022-04-09 14:49:53 -0400

    edit and update

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times { Article.create({ title: Faker::Company.catch_phrase, content: Faker::Company.bs }) }
